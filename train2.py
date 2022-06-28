569c44fd0f76b61c8d1c7bbcac7528573ded2e7f
Gaspard-a11y <gaspard.sagot@gmail.com>
2022-06-28 01:17:43 +0100

    WIP

--- train2.py
+++ train2.py
@@ -7,19 +7,22 @@ from modules.models import ArcFaceModel
 from modules.losses import SoftmaxLoss
 
 
-
 # TODO Load json instead
 config = {
     'epochs' : 10,
-    'learning_rate' : 5e-3
+    'learning_rate' : 5e-3,
+    'num_classes' : 10,
+    'embd_shape': 2,
+    'backbone_type' : 'Custom',
+    'head_type' : 'NormHead'
 }
 
 
 model = ArcFaceModel(input_size=28,
-                    backbone_type='Custom',
-                    num_classes=10,
-                    head_type='NormHead',
-                    embd_shape=2,
+                    backbone_type=config['backbone_type'],
+                    num_classes=config['num_classes'],
+                    head_type=config['head_type'],
+                    embd_shape=config['embd_shape'],
                     training=True)
 model.summary(line_length=80)
 
@@ -36,10 +39,10 @@ x_train = x_train/255.
 x_train = x_train[..., np.newaxis]
 
 train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-# test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
-
 train_dataset = train_dataset.shuffle(buffer_size)
 train_dataset = train_dataset.batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE)
+
+# test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
 # test_dataset = test_dataset.batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE)
 
 
@@ -51,4 +54,11 @@ model.compile(optimizer=optimizer, loss=loss_fn)
 model.fit(train_dataset,
             epochs=config['epochs'])
 
+
 print("[*] training done!")
+
+
+# TODO add ckpt saving
+# TODO re-load the model, this time in test mode
+# TODO examine the embeddings
+# TODO Re-do it all with Arcface head!
