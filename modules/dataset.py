518ba3f2fcb1da8a51c17d0f0dbf2f06d29c8385
Gaspard-a11y <gaspard.sagot@gmail.com>
2022-08-06 20:21:34 +0100

    WIP process MSD TFRecords

--- modules/dataset.py
+++ modules/dataset.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 import tensorflow as tf
 import numpy as np
 
@@ -18,8 +21,43 @@ def get_fashion_mnist_train_dataset(shuffle=True, buffer_size=1000):
     return train_dataset
 
 
-def get_MSD_train_dataset(shuffle=True, buffer_size=1000):
-    # TODO write me
+feature_description = {
+    'audio': tf.io.VarLenFeature(tf.float32),
+    'tags': tf.io.VarLenFeature(tf.string),
+    'tid': tf.io.VarLenFeature(tf.string),
+}
+
+
+def _waveform_parse_function(example_proto):
+    parsed_features = tf.io.parse_single_example(example_proto, feature_description)
+    
+    parsed_features['audio'] = tf.sparse.to_dense(parsed_features['audio'])
+    parsed_features['tid'] = tf.sparse.to_dense(parsed_features['tid'])
+    parsed_features['tags'] = tf.sparse.to_dense(parsed_features['tags'])
+    
+    return parsed_features
+
+
+def get_MSD_train_dataset(shuffle=True, buffer_size=1000, local=True):
+    # The sample rate used is 16kHz
+    if local==True:
+        waveforms_path = Path("./msd")
+    else:
+        # Training on Imperial College's Boden server
+        waveforms_path = Path("/srv/data/msd/tfrecords/waveform-complete")
+    
+    filenames = [f for f in os.listdir(waveforms_path) if f.endswith('tfrecord')]
+    
+    dataset = tf.data.TFRecordDataset(filenames)
+
+    dataset = dataset.map(_waveform_parse_function)
+
+    for data_example in dataset.take(1):
+        print(data_example['audio'])
+        print(data_example['tags'])
+        print(data_example['tid'])
+    # TODO finish
+
     return
 
 
