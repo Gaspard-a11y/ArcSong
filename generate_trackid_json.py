1846e9eaffefe91a5e59f4ba16642e6a1656f45f
Gaspard-a11y <gaspard.sagot@gmail.com>
2022-08-10 13:29:26 +0100

    Rename & avoid overwriting

---
+++ generate_trackid_json.py
@@ -0,0 +1,34 @@
+import os
+import json
+
+import fire
+from tqdm import tqdm
+
+from modules.dataset import get_MSD_train_dataset
+
+def generate_trackid_json(local=True, 
+                        overwrite = False, 
+                        out_path = 'msd/waveforms_track_ids.json'):
+    
+    if os.path.isdir(out_path) and not overwrite:
+        return
+    
+    else: 
+        dataset = get_MSD_train_dataset(local=local)
+        track_ids = {}
+        
+        for data_example in tqdm(dataset):
+            track_id = data_example['tid'].numpy()[0].decode('UTF-8')
+            try:
+                track_ids[track_id] += 1
+            except KeyError:
+                track_ids[track_id] = 1
+            
+        with open(out_path, 'w') as fp:
+            json.dump(track_ids, fp,  indent=4)
+        
+        return
+
+
+if __name__=="__main__":
+    fire.Fire(generate_trackid_json)
