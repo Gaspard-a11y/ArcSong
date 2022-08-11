f10035c0342e51c59d23c44d1a51d4aefa14336d
Gaspard-a11y <gaspard.sagot@gmail.com>
2022-08-11 16:46:47 +0100

    artist_name_to_song_count's keys are now sorted by song count

--- generate_trackid_artistname_dict.py
+++ generate_trackid_artistname_dict.py
@@ -7,8 +7,57 @@ import fire
 from modules.utils import save_json
 
 
-def main(overwrite = False,
-        out_path = "msd_data"):
+def process_echo_nest(echo_nest_metadata = 'msd_data\Echo_Nest_track_ids.txt'):
+    echo_nest_path=Path(echo_nest_metadata)
+    ### Load msd_data\Echo_Nest_track_ids.txt
+    f = open(echo_nest_path,'r', encoding="utf8")
+    lines = f.readlines()
+
+    def process_line(line):
+        track_id, song_id, artist_name, song_title = line.split('<SEP>')
+        return track_id, artist_name
+
+
+    ### Build dict[track_id]=artist_name, dict[artist_name]=number_of_songs
+
+    trackID_to_artistName = {}
+    artistName_to_songCount = {}
+
+    for line in tqdm(lines):
+        track_id, artist_name = process_line(line)
+        
+        # First dict
+        try:
+            _ = trackID_to_artistName[track_id]
+        except KeyError:
+            trackID_to_artistName[track_id] = artist_name
+
+        # Second dict
+        try:
+            artistName_to_songCount[artist_name] += 1
+        except KeyError:
+            artistName_to_songCount[artist_name] = 1
+    # TODO cleanup the artist names?
+
+
+    ### Build artist_list, dict[artist_name]=artist_number
+
+    artist_list = list(artistName_to_songCount.keys())
+    # Sort by descending number of tracks
+    artist_list.sort(key=artistName_to_songCount.__getitem__, reverse=True)
+
+    artistName_to_artistNumber = {}
+    for i, name in enumerate(artist_list):
+        artistName_to_artistNumber[name] = i
+
+    sorted_artistName_to_songCount = {}
+    for name in artist_list: # which is sorted
+        sorted_artistName_to_songCount[name] = artistName_to_songCount[name]
+
+    return trackID_to_artistName, sorted_artistName_to_songCount, artist_list, artistName_to_artistNumber
+
+
+def main(overwrite = False, out_path = "msd_data"):
     """
     Process the Echo_Nest_track_ids.txt file 
     from http://millionsongdataset.com/pages/getting-dataset/.
@@ -29,53 +78,20 @@ def main(overwrite = False,
         print("============ Found a json file, cancelling process ============")
         print("============= to overwrite, use: --overwrite True =============")
         return
+    
+    print("Processing Echo Nest metadata ...")
+    echo_nest = process_echo_nest()
+    trackID_to_artistName = echo_nest[0] 
+    artistName_to_songCount = echo_nest[1] 
+    artist_list = echo_nest[2] 
+    artistName_to_artistNumber = echo_nest[3]
 
-    ### Load msd_data\Echo_Nest_track_ids.txt
-    f = open('msd_data\Echo_Nest_track_ids.txt','r', encoding="utf8")
-    lines = f.readlines()
-
-    def process_line(line):
-        track_id, song_id, artist_name, song_title = line.split('<SEP>')
-        return track_id, artist_name
-
-
-    ### Build dict[track_id]=artist_name, dict[artist_name]=number_of_songs
-
-    trackID_to_artistName = {}
-    artistName_to_number_of_songs = {}
-
-    for line in tqdm(lines):
-        track_id, artist_name = process_line(line)
-        
-        # First dict
-        try:
-            _ = trackID_to_artistName[track_id]
-        except KeyError:
-            trackID_to_artistName[track_id] = artist_name
-
-        # Second dict
-        try:
-            artistName_to_number_of_songs[artist_name] += 1
-        except KeyError:
-            artistName_to_number_of_songs[artist_name] = 1
-    # TODO cleanup the artist names?
-
+    print("Saving ...")
     # Save dict[track_id] = artist_name
     save_json(trackID_to_artistName, out_path1)
     # Save dict[artist_name] = number_of_songs
-    save_json(artistName_to_number_of_songs, out_path2)
+    save_json(artistName_to_songCount, out_path2)
     
-
-    ### Build artist_list, dict[artist_name]=artist_number
-
-    artist_list = list(artistName_to_number_of_songs.keys())
-    # Sort by descending number of tracks
-    artist_list.sort(key=artistName_to_number_of_songs.__getitem__, reverse=True)
-
-    artistName_to_artistNumber = {}
-    for i, name in enumerate(artist_list):
-        artistName_to_artistNumber[name] = i
-
     # Save artist_list (ordered by number of )
     save_json(artist_list, out_path3)
     # Save dict[artist_name] = artist_number
