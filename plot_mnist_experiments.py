b70056285437a908d172d2799ca3b2054e7709de
Gaspard-a11y <gaspard.sagot@gmail.com>
2022-08-18 12:02:56 +0100

    MNIST experiments

--- plot_mnist_experiments.py
+++ plot_mnist_experiments.py
@@ -19,27 +19,12 @@ def main(out_dir="media", img=True,
     """
     Load the four trained models and save plotted embeddings.
     """
-
     # Load test dataset
     _, (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
     x_test = x_test/255.
 
     # Load configs
     configs = [config1, config2, config3, config4]
-    configs = [load_json(config) for config in configs]
-    
-    # Load models
-    print("Loading models and weights...")
-    models = [ArcModel(config=config, training=False) for config in configs]
-
-    # Load model weights
-    for k, config in enumerate(configs):
-        weights = tf.train.latest_checkpoint('./checkpoints/' + config['ckpt_name'])
-        models[k].load_weights(weights)
-        
-    print("Computing and plotting embeddings...")
-    # Compute embeddings
-    embeddings = [model(x_test).numpy() for model in models]
 
     # Plot untrained and trained encodings
     class_names = np.array(['T-shirt/top', 'Trouser/pants', 'Pullover shirt', 'Dress',
@@ -49,10 +34,21 @@ def main(out_dir="media", img=True,
     cmap = cm.get_cmap('jet', 10)
 
     for k in range(1,5): 
+        config = load_json(configs[k-1])
+    
+        # Load model
+        model = ArcModel(config=config, training=False)
+
+        # Load model weights
+        weights = tf.train.latest_checkpoint('./checkpoints/' + config['ckpt_name'])
+        model.load_weights(weights)
+            
+        # Compute embeddings
+        embds = model(x_test).numpy()
+        margin = config["margin"]
+        logist_scale = config["logist_scale"]
+
         plt.subplot(2, 2, k)
-        embds = embeddings[k-1]
-        margin = configs[k-1]["margin"]
-        logist_scale = configs[k-1]["logist_scale"]
         for i, class_label in enumerate(class_names):
             inx = np.where(y_test == i)[0]
             plt.scatter(embds[inx, 0], embds[inx, 1],
