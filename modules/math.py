073d991ae1fac92dfd0f4b9d3ccce5aa770c3d3e
Gaspard-a11y <gaspard.sagot@gmail.com>
2022-08-20 23:09:23 +0100

    Add two distance functions for embedding testing

---
+++ modules/math.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def euclidean_distance(x,y):
+    """
+    Return the euclidean distance of the two vectors.
+    """
+    return np.sqrt(np.sum(np.power(x-y,2)))
+
+
+def great_circle_distance(x,y, epsilon=1e-5):
+    """
+    Return the great circle distance of the two vectors.
+    """
+    norm_x = np.sqrt(np.sum(np.power(x,2)))
+    norm_y = np.sqrt(np.sum(np.power(y,2)))
+    xx, yy = x/(norm_x+epsilon), y/(norm_y+epsilon)
+    return 2*np.pi*np.arccos(np.dot(xx,yy))
+
