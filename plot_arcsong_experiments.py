72d7df9bb94b2a52477856932f3871ba45b00afd
Gaspard-a11y <gaspard.sagot@gmail.com>
2022-08-26 19:15:48 +0100

    New plots!

--- plot_arcsong_experiments.py
+++ plot_arcsong_experiments.py
@@ -7,6 +7,9 @@ import scipy as sp
 import seaborn as sns
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.metrics import confusion_matrix as sklearn_confusion_matrix
+from sklearn.metrics import auc
 
 from modules.utils import load_json
 from modules.models import ArcModel
@@ -88,14 +91,14 @@ def main(out_dir="media", network_config = None, dataset_config = None):
     print("Saving confusion matrix...")
     confusion_matrix = get_confusion_matrix(y_pred, y_test, num_classes=config["num_classes"])
     
-    plt.figure(figsize=(16,14))
+    plt.figure(figsize=(9,7))
     sns.heatmap(confusion_matrix, xticklabels=labels, yticklabels=labels)
     plt.yticks(rotation=30) 
     plt.title("Predictions on the test set (standard classification)")
     plt.xticks(rotation=30)
     plt.grid()
 
-    out_path = Path(out_dir) / "arcsong_confusion.png"
+    out_path = Path(out_dir) / "confusion_matrix.png"
     plt.savefig(out_path, bbox_inches='tight', pad_inches=0)
 
     ### Classification using latent distances
@@ -132,27 +135,144 @@ def main(out_dir="media", network_config = None, dataset_config = None):
     confusion_matrix_euclidean = get_confusion_matrix(y_pred_euclidean, y_test, num_classes=config["num_classes"])
     confusion_matrix_spherical = get_confusion_matrix(y_pred_spherical, y_test, num_classes=config["num_classes"])
 
-    plt.figure(figsize=(16,14))
+    plt.figure(figsize=(9,7))
     sns.heatmap(confusion_matrix_euclidean, xticklabels=labels, yticklabels=labels)
     plt.yticks(rotation=30) 
     plt.title("Predictions on the test set (euclidean distance in the latent space)")
     plt.xticks(rotation=30)
     plt.grid()
 
-    out_path = Path(out_dir) / "arcsong_confusion_euclidean.png"
+    out_path = Path(out_dir) / "confusion_matrix_euclidean.png"
     plt.savefig(out_path, bbox_inches='tight', pad_inches=0)
 
 
-    plt.figure(figsize=(16,14))
+    plt.figure(figsize=(9,7))
     sns.heatmap(confusion_matrix_spherical, xticklabels=labels, yticklabels=labels)
     plt.yticks(rotation=30) 
     plt.title("Predictions on the test set (spherical distance in the latent space)")
     plt.xticks(rotation=30)
     plt.grid()
 
-    out_path = Path(out_dir) / "arcsong_confusion_spherical.png"
+    out_path = Path(out_dir) / "confusion_matrix_spherical.png"
     plt.savefig(out_path, bbox_inches='tight', pad_inches=0)
     
+    ### 2-PCA of ID vectors
+
+    def normalise(vectors):
+        _, dim = vectors.shape
+        vectors = vectors - np.mean(vectors, axis=0)
+        vectors = np.divide(vectors, np.repeat(np.linalg.norm(vectors, ord=2, axis=1)[...,np.newaxis], dim, axis=1))
+        return vectors    
+
+    class_vectors_normalised = normalise(class_vectors)
+    pca = PCA(n_components=2)
+    vectors = pca.fit_transform(class_vectors_normalised)
+
+    vectors = class_vectors_normalised
+    plt.figure(figsize=(9,7))
+    for k in range(len(vectors)):
+        plt.plot([0, vectors[k][0]], [0, vectors[k][1]], label=labels[k])
+        plt.plot(vectors[k][0], vectors[k][1],'o', c='black')
+    plt.xlabel("2-PCA axis 1")
+    plt.ylabel("2-PCA axis 2")
+    plt.title("2-PCA of normalised identity vectors")
+    plt.legend()
+
+    out_path = Path(out_dir) / "2pca_identities.png"
+    plt.savefig(out_path, bbox_inches='tight', pad_inches=0)
+    
+    ### Binary classification
+
+    binary_y_test_matrix = np.array(
+        [[y_test[i]==y_test[j] for j in range(test_set_length)] for i in range(test_set_length)],
+        dtype=np.int32)
+
+    euclidean_embds_distance_matrix = np.zeros((test_set_length, test_set_length))
+    spherical_embds_distance_matrix = np.zeros((test_set_length, test_set_length))
+
+    for i in range(test_set_length):
+        for j in range(i):
+            euclidean_distance_value = euclidean_distance(embds[i], embds[j])
+            spherical_distance_value = spherical_distance(embds[i], embds[j])
+            euclidean_embds_distance_matrix[i][j] = euclidean_embds_distance_matrix[j][i] = euclidean_distance_value
+            spherical_embds_distance_matrix[i][j] = spherical_embds_distance_matrix[j][i] = spherical_distance_value
+
+    def return_stats(embds_distance_matrix, binary_y_test_matrix, threshold):
+        binary_y_pred_matrix = (embds_distance_matrix < threshold).astype(np.int32)
+        tn, fp, fn, tp = sklearn_confusion_matrix(
+            np.ndarray.flatten(binary_y_test_matrix), 
+            np.ndarray.flatten(binary_y_pred_matrix)).ravel()
+
+        fpr = fp/(fp+tn)
+        tpr = tp/(tp+fn)
+        f1 = 2*tp/(2*tp+fp+fn)
+        
+        return [fpr, tpr, f1]
+
+    ## Euclidean
+
+    max_distance_euclidean = np.amax(euclidean_embds_distance_matrix)
+    threshold_range_euclidean = np.linspace(start=0., stop=max_distance_euclidean, num=100)
+    euclidean_stats = np.array([return_stats(euclidean_embds_distance_matrix, binary_y_test_matrix, threshold) for threshold in threshold_range_euclidean])
+    euclidean_auroc_score = auc(euclidean_stats[:,0], euclidean_stats[:,1])
+
+    plt.figure(figsize=(16, 7))
+
+    plt.subplot(1, 2, 1)
+    plt.plot(euclidean_stats[:,0], euclidean_stats[:,1], label=f"ROC curve, area={euclidean_auroc_score:.2f}")
+    plt.plot([0,1], [0,1], linestyle="--")
+    plt.xlabel("False positive rate")
+    plt.ylabel("True positive rate")
+    plt.grid()
+    plt.legend()
+    plt.title("ROC curve of the binary classification")
+
+    plt.subplot(1, 2, 2)
+    plt.plot(threshold_range_euclidean, euclidean_stats[:,2])
+    plt.plot([threshold_range_euclidean[0], threshold_range_euclidean[-1]], 
+            [np.amax(euclidean_stats[:,2]), np.amax(euclidean_stats[:,2])], 
+            linestyle='--', label=f"Maximal F1-score: {np.amax(euclidean_stats[:,2]):.2f}")
+    plt.xlabel("Distance threshold")
+    plt.ylabel("F1-score")
+    plt.grid()
+    plt.legend()
+    plt.title("F1 score as a function of the distance threshold")
+
+    out_path = Path(out_dir) / "binary_classification_euclidean.png"
+    plt.savefig(out_path, bbox_inches='tight', pad_inches=0)
+
+    ## Spherical
+
+    max_distance_spherical = np.amax(spherical_embds_distance_matrix)
+    threshold_range_spherical = np.linspace(start=0., stop=max_distance_spherical, num=100)
+    spherical_stats = np.array([return_stats(spherical_embds_distance_matrix, binary_y_test_matrix, threshold) for threshold in threshold_range_spherical])
+    spherical_auroc_score = auc(spherical_stats[:,0], spherical_stats[:,1])
+
+    plt.figure(figsize=(16, 7))
+
+    plt.subplot(1, 2, 1)
+    plt.plot(spherical_stats[:,0], spherical_stats[:,1], label=f"ROC curve, area={spherical_auroc_score:.2f}")
+    plt.plot([0,1], [0,1], linestyle="--")
+    plt.xlabel("False positive rate")
+    plt.ylabel("True positive rate")
+    plt.grid()
+    plt.legend()
+    plt.title("ROC curve of the binary classification")
+
+    plt.subplot(1, 2, 2)
+    plt.plot(threshold_range_spherical, spherical_stats[:,2])
+    plt.plot([threshold_range_spherical[0], threshold_range_spherical[-1]], 
+            [np.amax(spherical_stats[:,2]), np.amax(spherical_stats[:,2])], 
+            linestyle='--', label=f"Maximal F1-score: {np.amax(spherical_stats[:,2]):.2f}")
+    plt.xlabel("Distance threshold")
+    plt.ylabel("F1-score")
+    plt.grid()
+    plt.legend()
+    plt.title("F1 score as a function of the distance threshold")
+
+    out_path = Path(out_dir) / "binary_classification_spherical.png"
+    plt.savefig(out_path, bbox_inches='tight', pad_inches=0)
+
     return
 
 
